c8b7cdd358c4f063b4aac21d5513a70e3e715348
daniel john jacob <danieljj1805@gmail.com>
2025-03-05 19:58:49 +0530

    added method to find specific card

--- Deck.java
+++ Deck.java
@@ -37,5 +37,24 @@ class Deck {
         }
     }
 
-    
+    // Method to check if two cards belong to the same suit
+    public boolean sameCard(Card c1, Card c2) {
+        return c1.getSuit().equals(c2.getSuit());
+    }
+
+    // Method to check if two cards have the same rank
+    public boolean compareCard(Card c1, Card c2) {
+        return c1.getRank().equals(c2.getRank());
+    }
+
+     // Method to find a specific card
+    public boolean findCard(String rank, String suit) {
+        for (Card card : deck) {
+            if (card.getRank().equalsIgnoreCase(rank) && card.getSuit().equalsIgnoreCase(suit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
