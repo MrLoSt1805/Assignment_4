b4dfc2151e54cd535fcce6375b9581e9a2f8d88a
daniel john jacob <danieljj1805@gmail.com>
2025-03-05 19:55:55 +0530

    added Deck.java with method to create deck of 52 cards

---
+++ Deck.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+
+    }
+
+
+    // Method to create a deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+
+
+}
